8518010fc0300afd8daae81e355af45c4e9de000
vincent vk <12504472@student.pxl.be>
2025-11-06 13:24:15 +0100

    test

---
+++ DemoVereisteAnalyse/DemoVereisteAnalyse/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DemoVereisteAnalyse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                const string user = "Admin", password = "P@sswoord123";
+                int poging = 0;
+                while (poging < 3)
+                {
+                    Console.Write("Gebruikersnaam: ");
+                    string Gebruiker = Console.ReadLine();
+                    Console.Write("Wachtwoord: ");
+                    string inputPassword = Console.ReadLine();
+                    if (Gebruiker == user && inputPassword == password)
+                    {
+                        Console.WriteLine($"Welkom, {Gebruiker}!");
+                        return;
+                    }
+                    poging++;
+                    Console.WriteLine($"login is fout.{3 - poging} pogingen over.");
+                }
+                Console.WriteLine("Te veel pogingen. Toegang geweigerd.");
+            }
+        }
+    }
+
